84e4e4270e6cc3f921b92b6648e324aa3ebf9924
97gupe30 <97gupe30@gmail.com>
2015-01-11 20:35:44 +0100

    V0.1
    Changes:
    - Added Health Points to the player (100HP).
    - Added a wallet.
    - Changed the background.
    - Changed the color of the shop.
    
    Fixes:
    - 'tower1' is now fully working.
    - Fixed a bug where the last enemy couldn't die.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var level = 1;
-var enemies = [[], 1, 0, 5, 0]; // INDEX 0 = Hur många fiender som är ute på planen just nu. INDEX 1 = Hur många finder som ska komma på denna leveln. INDEX 2 = Unikt ID.
+var enemies = [[], 3, 0, 2.5, 0]; // INDEX 0 = Fiende object INDEX 1 = Hur många finder som ska komma på denna leveln. INDEX 2 = Antal fiender ute på planen. INDEX 3 = Fiendens hastighet. INDEX 4 = Används till att röra tornen
 var genEnemy = true;
 var hp = 100;
 var moveActive = false;
 var towers = [];
+var towerCount = 0;
+var money = 100;
 
 function init() {
     game = document.getElementById('camera');
@@ -14,17 +16,6 @@ function init() {
     window.setInterval(generate, 1000);
 }
 
-function mapLayout() {
-    if(level == 1) {
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 100, 900, 50);
-        ctx.fillRect(100, 300, 800, 50);
-        ctx.fillRect(850, 100, 50, 100);
-        ctx.fillRect(100, 250, 50, 100);
-        ctx.fillRect(900, 200, -800, 50);
-        ctx.fillRect(850, 300, 50, 200);
-    }
-}
 
 function keyHandler(event) {
     if(moveActive) {
@@ -43,10 +34,11 @@ function keyHandler(event) {
     }
 }
 
-function Enemy(x, y, Ehp) {
+function Enemy(x, y, Ehp, id) {
     this.x = x;
     this.y = y;
-    this.hp = Ehp
+    this.hp = Ehp;
+    this.id = id;
 
     this.render = function() {
         ctx.fillStyle = "black";
@@ -79,16 +71,24 @@ function Enemy(x, y, Ehp) {
     }
 
     this.die = function() {
-        enemies[0].splice(0, 1);
+        // Denna for-loopen kollar igen alla ID i arrayen enemies [0] och kollar ifall ID:en är samma på den som vi är på just nu. Ifall den är det så tar den bort den från arrayen och fienden har dött.
+        for(var i = 0; i < enemies[0].length; i++) { 
+            if(enemies[0][i].id === this.id) {
+                enemies[0].splice(i, 1);
+            }
+        }
+
     }
 }
 
 
 // KÖP SAKER
-function Tower(x, y, type) {
+function Tower(x, y, type, id) { // CD == Om tornet kan skada fiender
     this.x = x;
     this.y = y;
     this.type = type;
+    this.id = id;
+    this.cd = true;
 
     this.render = function() {
         if(this.type == 'tower1') {
@@ -98,31 +98,47 @@ function Tower(x, y, type) {
             img.height = 50;
             ctx.beginPath();
             ctx.drawImage(img, this.x, this.y);
-            ctx.arc(this.x + 25, this.y + 25, 60, 0, 2*Math.PI);
+            ctx.arc(this.x + 25, this.y + 25, 70, 0, 2*Math.PI);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "black";
             ctx.stroke();
             ctx.closePath();
 
         }
     }
-    
-    this.attack = function() {
+
+    this.attack = function(j) {
         var dx, dy, d;
         if(this.type == 'tower1') {
-            for(var i = 0; i < enemies[0].length; i++) {
-                dx = enemies[0][i].x - this.x + 25;
-                dy = enemies[0][i].y - this.y + 25;
+            for(var i = 0; i < enemies[0].length; i++) { // Räknar ut om fienden är inom tornets Attack Range.
+                dx = enemies[0][i].x - this.x - 25;
+                dy = enemies[0][i].y - this.y - 25;
                 d = Math.sqrt(dx*dx + dy*dy);
-                console.log(d);
-                if(d <= 60) {
-                    alert(1);
+                if(d <= 70) { // Ifall fienden är inom tornets Attack Range målar vi ut skottet och lägger till en CD.
+                    this.attackAnimation(i);
+
+                    if(this.cd) { // Lägger till en 0.5 sekunds CD på tornet
+                        enemies[0][i].hp -= 3;
+                        console.log(enemies[0][i].hp);
+                        this.cd = false;
+                        setTimeout(function() {
+                            towers[j].cd = true;
+                        }, 500);
+                    }
                 }
             }
         }
     }
-}
 
-function tower1() {
-    towers.push(new Tower(0, 50, 'tower1'));
+    this.attackAnimation = function(i) {
+        ctx.beginPath(); // Målar ut laser stråle
+        ctx.moveTo(this.x + 25, this.y + 25);
+        ctx.lineTo(enemies[0][i].x, enemies[0][i].y);
+        ctx.closePath();
+        ctx.strokeStyle = "purple";
+        ctx.lineWidth = 5;
+        ctx.stroke();
+    }
 }
 
 function getPosition(event) {
@@ -139,27 +155,38 @@ function getPosition(event) {
     }
 }
 
+function buyTower(type, cost) {
+    if(money >= cost) {
+        towers.push(new Tower(0, 50, type, towerCount));
+        money -= cost;
+    }
+    towerCount++;
+}
+
 
 function animate() {
     ctx.clearRect(0, 0, 1000, 500);
-    mapLayout();
     if(level === 1) {
         for(var i = 0; i < enemies[0].length; i++) {
             enemies[0][i].render();
             enemies[0][i].walk();
+            if(enemies[0][i].hp <= 0) {
+                enemies[0][i].die();
+            }
         }
     }
-    for(var i = 0; i < towers.length; i++) {
-        towers[i].render();
-        towers[i].attack();
+    for(var j = 0; j < towers.length; j++) {
+        towers[j].render();
+        towers[j].attack(j);
     }
 
     document.getElementById('hp').innerHTML = "HP: " + hp;
+    document.getElementById('money').innerHTML = "Kassa: " + money + "kr";
 }
 
 function generate() {
     if(enemies[0].length < enemies[1] && genEnemy) {
-        enemies[0].push(new Enemy(0, 125, 10));
+        enemies[0].push(new Enemy(0, 125, 10, enemies[2]));
         enemies[2]++;
         if(enemies[2] == enemies[1]) {
             genEnemy = false;
