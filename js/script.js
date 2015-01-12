f30401ea1baea53257af4ffe520a3d42e20c91d4
Vezel <Gustav@Gustavs-MacBook-Air.local>
2015-01-12 12:50:04 +0100

    V0.12A
    Changes:
    - Shop has a icon for tower1
    - Added 3 menus.
    - Added waves
    
    Fixes:
    - Fixed bug where enemies wouldn’t change x and y after speed > 5.
    -

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 var level = 1;
-var enemies = [[], 100, 0, 5, 0]; // INDEX 0 = Fiende object INDEX 1 = Hur många finder som ska komma på denna leveln. INDEX 2 = Antal fiender ute på planen. INDEX 3 = Fiendens hastighet. INDEX 4 = Används till att röra tornen
+var enemies = [[], 3, 0, 5, 0]; // INDEX 0 = Fiende object INDEX 1 = Hur många finder som ska komma på denna leveln. INDEX 2 = Antal fiender ute på planen. INDEX 3 = Fiendens hastighet. INDEX 4 = Används till att röra tornen
 var towers = [];
 var menus = [new Menu('dead'),
-             new Menu('restart')
+             new Menu('restart'),
+             new Menu('pause'),
+             new Menu('waveClear')
             ];
+var menuP = 'pause';
 var towerCount = 0;
+var currentEnemies = [];
 
 // Värde variabler
 var hp = 100;
@@ -13,7 +17,9 @@ var money = 100;
 // Booleans
 var genEnemy = true; // Ifall man ska kunna generera nya fiender.
 var moveActive = false; // Ifall man kan flytta torn eller inte.
-var confirm = false;
+var confirm = false; // Till reset menyn
+var confirmWave = false;
+var confirmMove; // Till menyer.
 
 // timer variabler;
 var infoBox;
@@ -23,7 +29,7 @@ function init() {
     game = document.getElementById('camera');
     ctx = game.getContext('2d');
     game.addEventListener("mousedown", getPosition, false);
-                                                        
+
     window.setInterval(animate, 25);
     window.setInterval(generate, 1000);
 }
@@ -31,7 +37,7 @@ function init() {
 
 function keyHandler(event) {
     var key = event.keyCode;
-    if(moveActive) {
+    if(moveActive) { // Flytta torn
         if(key == 65) {
             towers[enemies[4]].x -= 50;
         } else if(key == 68) {
@@ -44,8 +50,44 @@ function keyHandler(event) {
             moveActive = false;
         }
     }
-    if(key == 82) {
-        document.getElementById('infoBox').innerHTML = 'Are you sure you want to restart?';
+    if(key == 82) { // "R", Starta reset menyn
+        document.getElementById('infoBox').innerHTML = 'Are you sure you want to restart?<br><br><span id="yes">YES</span> <span id="no">NO</span>';
+        document.getElementById('infoBox').style.top = '450px';
+        document.getElementById('no').style.backgroundColor= 'whitesmoke';
+        confirm = true;
+        confirmMove = 'right';
+    }
+    if(confirm) { // Reset menyn.
+        if(key == 37) {
+            confirmMove = 'left';
+            document.getElementById('no').style.backgroundColor = '';
+            document.getElementById('yes').style.backgroundColor = 'whitesmoke';
+        } else if(key == 39) {
+            confirmMove = 'right';
+            document.getElementById('yes').style.backgroundColor = "";
+            document.getElementById('no').style.backgroundColor= 'whitesmoke';
+        } else if(key == 13) {
+            confirm = false;
+            document.getElementById('infoBox').style.top = '480px';
+            if(confirmMove == 'left') {
+                menus[1].startMenu();
+            } else {
+                document.getElementById('infoBox').innerHTML = "";
+            }
+        }
+    }
+    if(key == 80 && confirmWave != true) {
+        menus[2].startMenu();
+    }
+    if(confirmWave) {
+        if(key == 80) {
+            document.getElementById('infoBox').innerHTML = "";
+            enemies[1] += 3;
+            enemies[2] = 0;
+            enemies[3] += 1;
+            genEnemy = true;
+            confirmWave = false;
+        }
     }
 }
 
@@ -67,21 +109,21 @@ function Enemy(x, y, Ehp, id) {
 
 
     this.walk = function() {
-        if(this.x < 875 && this.y == 325) {
+        if(this.x < 875 && this.y < 340 && this.y > 320) {
             this.x += enemies[3];
-        } else if(this.x <= 875 && this.x > 125 && this.y == 225) {
+        } else if(this.x <= 900 && this.x > 125 && this.y >= 225 && this.y <= 250) {
             this.x -= enemies[3];
-        } else if(this.x < 875 && this.y == 125) {
+        } else if(this.x < 875 && this.y >= 125 && this.y <= 130) {
             this.x+= enemies[3];
-        } else if(this.y > 100 && this.y < 225 && this.x == 875) {
+        } else if(this.y > 100 && this.y < 225 && this.x >= 875) {
             this.y+= enemies[3];
-        } else if(this.y > 200 && this.y < 325 && this.x == 125) {
+        } else if(this.y > 200 && this.y < 325 && this.x <= 125) {
             this.y += enemies[3];
-        } else if(this.x == 875 && this.y > 290) {
+        } else if(this.x >= 875 && this.y > 290) {
             this.y += enemies[3];
         }
 
-        if(this.x === 875 && this.y === 520) {
+        if(this.x >= 875 && this.y >= 520) {
             hp -= 5;
             this.die();
         }
@@ -92,6 +134,9 @@ function Enemy(x, y, Ehp, id) {
         for(var i = 0; i < enemies[0].length; i++) { 
             if(enemies[0][i].id === this.id) {
                 enemies[0].splice(i, 1);
+            }
+            if(enemies[0].length == 0 && genEnemy == false) { // När waven är klar.
+                menus[3].startMenu();
             }
         }
 
@@ -136,7 +181,9 @@ function Tower(x, y, type, id) { // CD == Om tornet kan skada fiender
 
                     if(this.cd) { // Lägger till en 0.5 sekunds CD på tornet
                         enemies[0][i].hp -= 3;
-                        console.log(enemies[0][i].hp);
+                        if(enemies[0][i].hp <= 0) {
+                            money += 3;
+                        }
                         this.cd = false;
                         cdTimeout = setTimeout(function() {
                             towers[j].cd = true;
@@ -150,7 +197,7 @@ function Tower(x, y, type, id) { // CD == Om tornet kan skada fiender
     this.attackAnimation = function(i) {
         ctx.beginPath(); // Målar ut laser stråle
         ctx.moveTo(this.x + 25, this.y + 25);
-        ctx.lineTo(enemies[0][i].x, enemies[0][i].y);
+        ctx.lineTo(enemies[0][i].x + 6.25, enemies[0][i].y + 6.25);
         ctx.closePath();
         ctx.strokeStyle = "#000055";
         ctx.lineWidth = 2;
@@ -201,13 +248,38 @@ function Menu(type) {
                 towers[i].cd = false;
             }
         } else if(this.type == 'restart') {
-            enemies = [[], 100, 0, 5, 0];
+            document.getElementById('infoBox').innerHTML = "";
+            enemies = [[], 3, 0, 5, 0];
             towers = [];
             level = 1;
             hp = 100;
             money = 100;
             genEnemy = true;
             moveActive = false;
+            confirm = false;
+        } else if(this.type == 'pause') {
+            if(menuP == 'pause') {
+                menuP = 'unpause';
+                document.getElementById('infoBox').innerHTML = "Press 'P' to unpause.";
+                for(var i = 0; i < enemies.length; i++) {
+                    currentEnemies[i] = enemies[i];
+                }
+                enemies[3] = 0;
+                moveActive = false;
+                genEnemy = false;
+            } else if(menuP == 'unpause') {
+                document.getElementById('infoBox').innerHTML = "";
+                menuP = 'pause';
+                for(var i = 0; i < currentEnemies.length; i++) {
+                    enemies[i] = currentEnemies[i];
+                }
+                if(enemies[1] != enemies[2]) {
+                    genEnemy = true;
+                }
+            }
+        } else if(this.type == 'waveClear') {
+            document.getElementById('infoBox').innerHTML = "Press 'P' to start the next wave.";
+            confirmWave = true;
         }
     }
 }
@@ -215,10 +287,10 @@ function Menu(type) {
 
 function animate() {
     ctx.clearRect(0, 0, 1000, 500);
-    if(level === 1) {
-        for(var i = 0; i < enemies[0].length; i++) {
-            enemies[0][i].render();
-            enemies[0][i].walk();
+    for(var i = 0; i < enemies[0].length; i++) {
+        enemies[0][i].render();
+        enemies[0][i].walk();
+        if(enemies[0].length > 0) { 
             if(enemies[0][i].hp <= 0) {
                 enemies[0][i].die();
             }
@@ -229,10 +301,12 @@ function animate() {
         towers[j].attack(j);
     }
 
-
     if(hp <= 0) {
-        menus[0].startMenu();
+        if(confirm != true) {
+            menus[0].startMenu();
+        }
     }
+
     document.getElementById('hp').innerHTML = hp + "HP";
     document.getElementById('money').innerHTML = "Wallet: " + money + "kr";
 }
